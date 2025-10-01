185c82d909e4b4a28f7ab9f74643539b5561e9cf
Shashwat Deshpande <shashwatd165@gmail.com>
2025-10-01 12:00:49 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,22 @@
 import express from 'express'
+import path from 'path'
 const app = express()
 import dotenv from 'dotenv'
 dotenv.config()
+const __dirname = path.resolve()
+
+
 const PORT=process.env.PORT || 3000
 import authroutes from './routes/auth.route.js'
 import messageroutes from './routes/message.route.js'
 app.use("/api/auth", authroutes)
 app.use("/api/messages", messageroutes)
-
-
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/ff/dist')))
+    app.get(/.*/, (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/ff/dist/index.html'));
+    })
+}
 app.listen(PORT , () => {
     console.log(`Example app listening on port: ${PORT}`);
 })
